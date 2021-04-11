ad7fcd916f8b6771a5ec08a1381e7b0bdbdb4e68
Miguel <miguelantoliv@gmail.com>
2021-04-11 15:34:37 -0300

    feat(Dependency Injection): Create dependency injection process.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
+import 'injection_container.dart' as di;
 
-void main() => runApp(MaterialApp(
-  home: Container(),
-));
+void main() async {
+  await di.init();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AMOC',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: Container(),
+    );
+  }
+}
