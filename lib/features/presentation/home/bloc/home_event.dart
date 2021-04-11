5b526fc12170ad4f6e244e94104143c8737a229d
Miguel <miguelantoliv@gmail.com>
2021-04-11 14:55:26 -0300

    test(HomeEvent): Create tests for HomeEvent classes.

--- lib/features/presentation/home/bloc/home_event.dart
+++ lib/features/presentation/home/bloc/home_event.dart
@@ -2,9 +2,9 @@ part of 'home_bloc.dart';
 
 abstract class HomeEvent extends Equatable {
   const HomeEvent();
+}
 
+class GetAccountsEvent extends HomeEvent {
   @override
   List<Object> get props => [];
 }
-
-class GetAccountsEvent extends HomeEvent {}
