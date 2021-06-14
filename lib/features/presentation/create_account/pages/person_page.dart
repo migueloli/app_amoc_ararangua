c2272b787514bedc629a73a386cd5a567a0e13ea
Miguel <miguelantoliv@gmail.com>
2021-06-13 23:45:01 -0300

    feat: Update person page.

--- lib/features/presentation/create_account/pages/person_page.dart
+++ lib/features/presentation/create_account/pages/person_page.dart
@@ -1,4 +1,8 @@
+import '../widgets/dropdown_widget.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../core/validators/validators.dart';
+import '../../create_account/widgets/text_field_widget.dart';
 
 class PersonPage extends StatelessWidget {
   final GlobalKey<FormState> _form = GlobalKey();
@@ -9,7 +13,36 @@ class PersonPage extends StatelessWidget {
       key: _form,
       child: Column(
         children: [
-
+          TextFieldWidget(
+            label: "Descrição",
+            icon: Icon(Icons.description),
+            inputType: TextInputType.multiline,
+            minLines: 3,
+            onSaved: (value) {},
+            textInputAction: TextInputAction.newline,
+            value: '',
+            validator: validateDescription,
+          ),
+          DropdownWidget<String>(
+            initialValue: '',
+            onChanged: (value) {},
+            items: [].map(
+              (e) => DropdownMenuItem<String>(
+                value: e.description,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(Icons.category,),
+                    SizedBox(width: 12,),
+                    Text(
+                      e.description.toUpperCase(),
+                    ),
+                  ],
+                ),
+              )
+            ).toList(),
+          ),
         ],
       )
     );
