247382c65354c57c6bd3c185ae92b1eb532a420d
Miguel <miguelantoliv@gmail.com>
2021-06-13 23:15:33 -0300

    feat: Create the modules routes for create account and about.

--- lib/features/presentation/app/app_module.dart
+++ lib/features/presentation/app/app_module.dart
@@ -5,6 +5,8 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 import '../../../core/network/network_info_implementation.dart';
 import '../../data/datasources/accounts_remote_data_source.dart';
 import '../../data/repositories/accounts_repository_implementation.dart';
+import '../about/about_module.dart';
+import '../create_account/create_account_module.dart';
 import '../home/home_module.dart';
 
 class AppModule extends Module {
@@ -15,7 +17,7 @@ class AppModule extends Module {
     Bind((i) {
       var dio = Dio(
         BaseOptions(
-          baseUrl: 'http://192.168.0.103:8081/api/v1/',
+          baseUrl: 'http://192.168.0.100:8081/api/v1/',
           headers: {
             Headers.contentTypeHeader: Headers.jsonContentType
           }
@@ -35,6 +37,8 @@ class AppModule extends Module {
   @override
   List<ModularRoute> get routes => [
     ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ModuleRoute(Modular.initialRoute, module: CreateAccountModule()),
+    ModuleRoute(Modular.initialRoute, module: AboutModule()),
   ];
 
 }
