023943222f9bcc561fb402f4490e7a6e21eac563
Miguel <miguelantoliv@gmail.com>
2021-08-04 10:54:46 -0300

    feat: Create custom exceptions and failures for login.

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -15,3 +15,80 @@ class NetworkFailure extends Failure {
   List<Object> get props => [];
 
 }
+
+class LoginFailure extends Failure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class InvalidEmailFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class OperationNotAllowedFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class WeakPasswordFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class UserDisabledFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class UserNotFoundFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class WrongPasswordFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class AccountExistsWithDifferentCredentialFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class InvalidCredentialFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class InvalidVerificationCodeFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class InvalidVerificationIdFailure extends LoginFailure {
+
+  @override
+  List<Object> get props => [];
+
+}
