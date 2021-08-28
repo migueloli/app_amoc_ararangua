0a6d210a833a82535abd62ea622005c470f27e16
Miguel <miguelantoliv@gmail.com>
2021-08-28 15:34:35 -0300

    feature: Add crashlytics.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'features/presentation/app/app_module.dart';
 import 'features/presentation/app/app_widget.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(
-    ModularApp(
-      module: AppModule(),
-      child: AppWidget(),
-    )
-  );
+  runZonedGuarded(mainRunApp, FirebaseCrashlytics.instance.recordError);
 }
+
+void mainRunApp() => runApp(
+  ModularApp(
+    module: AppModule(),
+    child: AppWidget(),
+  )
+);
