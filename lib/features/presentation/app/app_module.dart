d9310d3415192e4ac27df3d1533a28b55a70b90c
Miguel <miguelantoliv@gmail.com>
2021-10-06 21:11:30 -0300

    refactor: Create process for category.

--- lib/features/presentation/app/app_module.dart
+++ lib/features/presentation/app/app_module.dart
@@ -6,9 +6,14 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 import '../../../core/network/network_info_implementation.dart';
 import '../../data/datasources/accounts_remote_datasource_implementation.dart';
+import '../../data/datasources/categories_remote_datasource_implementation.dart';
 import '../../data/repositories/accounts_repository_implementation.dart';
+import '../../data/repositories/categories_repository_implementation.dart';
+import '../create_account/create_account_module.dart';
 import '../home/home_module.dart';
 import '../login/login_module.dart';
+import '../profile/profile_module.dart';
+import 'bloc/category_bloc.dart';
 
 class AppModule extends Module {
   @override
@@ -20,12 +25,16 @@ class AppModule extends Module {
     Bind((i) => GoogleSignIn()),
     Bind((i) => AccountsRemoteDataSourceImplementation(i.get(), i.get(), i.get())),
     Bind((i) => AccountsRepositoryImplementation(i.get(), i.get())),
+    Bind((i) => CategoriesRemoteDataSourceImplementation(i.get())),
+    Bind((i) => CategoriesRepositoryImplementation(i.get(), i.get())),
+    Bind((i) => CategoryBloc(i.get())),
   ];
 
   @override
   List<ModularRoute> get routes => [
     ModuleRoute(Modular.initialRoute, module: HomeModule()),
     ModuleRoute("${Modular.initialRoute}login", module: LoginModule()),
+    ModuleRoute("${Modular.initialRoute}create-account", module: CreateAccountModule()),
+    ModuleRoute("${Modular.initialRoute}profile", module: ProfileModule()),
   ];
-
 }
